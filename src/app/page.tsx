15fd81d3ecea4f3d86331e00c04dea19536ebf7e
Ikram Akbar <ikramakbar71@gmail.com>
2024-11-29 19:28:11 +0600

    MUI installed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import { Button } from "@mui/material";
 
 
 const Home = () => {
   return (
     <div>
       <h1>Home page ! MY NEXT JS</h1>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
     </div>
   );
 };
